ea2f3df81b4251d4218093fe1e3cf18bbd45f3e5
adiel-sml <leolinsanga@gmail.com>
2026-08-12 23:29:37 +0000

    Align default port with env config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const pool = require('./db');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 
 app.use(express.json());
 
